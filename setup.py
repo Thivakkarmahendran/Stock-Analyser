87b4b3d75d7dad0fbfc25c31e9f5ee53ceda6145
Thivakkar Mahendran <mthivakkar@gmail.com>
2022-05-03 19:12:04 -0400

    change code structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='stock-analyser',
+   version='0.1.0',
+   description='Scrap the internet about stocks',
+   author='Thivakkar Mahendran',
+   author_email='mthivakkar@gmail.com',
+   packages=['stock-analyser'] 
+)
