d7d8d61b70ee092a25742dffd7f168446db9b3bd
Thivakkar Mahendran <mthivakkar@gmail.com>
2022-05-16 22:29:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,90 +1,16 @@
 #imports
-from os.path import exists
-import os
-import pandas as pd
 import time
-import warnings
 
-from stock_analyser.redditAPI import redditTimeFilter
-from stock_analyser.redditAPI import redditAPI
-from stock_analyser.twitterAPI import twitterAPI
-from stock_analyser.stockExtractor import stockExtractor
-from stock_analyser.stockAnalysis import getTopStocks, printSentimalAnalysis
-from stock_analyser.sentimalAnalysis import stockTextSentimentAnalysis
-from config import *
+from stock_analyser.redditAnalysis import runRedditAnalysis
 
-
-#setup
-warnings.filterwarnings("ignore")
-
-
+startTime = time.time()    
 
 def main():
     
     print("***** Starting Stock Analyser ***** \n")
     
-    startTime = time.time()    
-    lastTaskTime = time.time()  
-
-    os.makedirs('dataset', exist_ok = True)
-    
-
-    #get Reddit Titles
-    dfTitles = pd.DataFrame()
-    if not exists("dataset/redditTitles.csv") or (startTime - os.path.getmtime("dataset/redditTitles.csv") > DATA_REFRESH):
-        
-        os.remove("dataset/redditTitles.csv")
-        os.remove("dataset/redditComments.csv")
-        
-        redditApi = redditAPI()
-        
-        for subreddit in REDDIT_SUBREDDITS:
-            dfTitles = dfTitles.append(redditApi.getTopSubredditTitles(subreddit, redditTimeFilter.WEEK.value))
-        dfTitles.to_csv("dataset/redditTitles.csv", index = False)
-    else:
-        print("Reddit Titles dataset already exists")
-    
-    print("Done getting reddit Titles: {} seconds".format(time.time()-startTime))
-    lastTaskTime = time.time()
-        
-        
-    #get Comments    
-    if not exists("dataset/redditComments.csv"):
-        redditApi = redditAPI()
-        dfTitles = pd.read_csv("dataset/redditTitles.csv")
-        dfAllComments = pd.DataFrame()
-        
-        for index, redditTitle in dfTitles.iterrows():
-           dfAllComments = dfAllComments.append(redditApi.getPostComments(redditTitle['postId'], redditTitle['subredditName']))
-
-        dfAllComments.to_csv("dataset/redditComments.csv", index = False)
-    else:
-        print("Reddit Comments dataset already exists")
-    
-    print("Done getting reddit comments: {} seconds".format(time.time()-startTime))
-    
-    #extract stocks from comments
-    dfAllComments = pd.read_csv("dataset/redditComments.csv")
-    stockExtract = stockExtractor()
-    stocks, stockTexts = stockExtract.getStockCountFromDF(dfAllComments['commentText'])
-    
-    print("Done getting stocks from comments: {} seconds".format(time.time()-lastTaskTime))
-    lastTaskTime = time.time()
-    
-    #get Top Stocks
-    topStocks, topStocksAndCount = getTopStocks(stocks)
-    
-    print(topStocksAndCount)
-    
-    print("Done getting top stocks: {} seconds".format(time.time()-lastTaskTime))
-    lastTaskTime = time.time()
-    
-    stockScores = stockTextSentimentAnalysis(topStocks, stockTexts)
-    
-    print("Done perfoming sentiment analysis on top stocks: {} seconds".format(time.time()-lastTaskTime))
-    
-    
-    printSentimalAnalysis(topStocks, stockScores)
+    #Analysis Reddit 
+    runRedditAnalysis()
     
     print("***** Completed Stock Analyser in {} seconds ***** \n".format(time.time()-startTime))
     
